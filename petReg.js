76d1997531ccbe8cc1f0a47bd08dd9e9612d33fc
sle3pyy <simaomarquesantos@gmail.com>
2023-12-19 12:53:43 +0000

    megh

--- petReg.js
+++ petReg.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
     $('form').on('submit', function(event) {
         console.log()
-        event.preventDefault(); // Prevent the form from submitting normally
+        event.preventDefault();
+        var formData = {}; 
 
-        var formData = {}; // Initialize an empty object to hold the form data
-
-        // Iterate over each input field in the form
         $(this).find('input').each(function() {
-            // If the input field is a file input
             if (this.type === 'file') {
                 // Get the first file from the files property
                 var file = this.files[0];
@@ -15,32 +12,23 @@ $(document).ready(function() {
                 if (file) {
                     // Create a new FileReader object
                     var reader = new FileReader();
-                
-                    // Store a reference to the file input element
-                    var fileInput = $(this);
-                
                     // Read the file as a data URL
                     reader.readAsDataURL(file);
                 
                     // When the file is loaded
                     reader.onload = function(e) {
                         // Store the image data in formData using the stored reference to the file input element
-                        formData[$(this).attr('id')] = e.target.result;
+                        formData["photo"] = e.target.result;
                 
                     };
                 }
             } else {
-                // Add the input field's id as the key and its value as the value to the formData object
                 formData[$(this).attr('id')] = $(this).val();
             }
+            console.log(formData)
         });
-        // Get the existing data from local storage
         var existingData = JSON.parse(localStorage.getItem('accounts')) || [];
-
-        // Get the logged-in email
         var loggedInEmail = localStorage.getItem('loggedInEmail');
-
-        // Find the logged-in account
         var account = existingData.find(account => account.email === loggedInEmail);
 
         if (!account) {
@@ -54,19 +42,18 @@ $(document).ready(function() {
             return;
         }
 
-        // Add the form data to the animals array of the account
         if (!account.animals) {
             account.animals = [];
         }
         
         account.animals.push(formData);
 
-        // Store the updated accounts back in local storage
         localStorage.setItem('accounts', JSON.stringify(existingData));
 
         console.log(account)
         console.log(JSON.parse(localStorage.getItem('accounts')));
     });
+});
 
     $('#clearAnimals').on('click', function() {
         // Get the existing data from local storage
@@ -92,13 +79,15 @@ $(document).ready(function() {
         console.log(account);
         console.log(JSON.parse(localStorage.getItem('accounts')));
     });
-    $('#clearImages').on('click', function() {
-        localStorage.removeItem('uploadedImage')
-        ;});
+   
+    
+
+$('#clearImages').on('click', function() {
+    localStorage.removeItem('uploadedImage')
+    ;});
     $('#logAccounts').on('click', function() {
         // Get the existing data from local storage
         var existingData = JSON.parse(localStorage.getItem('accounts')) || [];
 
         console.log(existingData);
     });
-});
