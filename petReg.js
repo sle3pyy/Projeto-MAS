59e4217ab3e0700c8aba170a0f2f5c35e4476db6
sle3pyy <simaomarquesantos@gmail.com>
2023-12-19 19:16:58 +0000

    BINGUS

--- petReg.js
+++ petReg.js
@@ -2,12 +2,38 @@ var vm= function(){
     var self = this;
     self.animals = ko.observableArray([]);
     self.account = ko.observableArray([]);
+    self.loggedInEmail = ko.observable(localStorage.getItem('loggedInEmail'));
+    var existingData = JSON.parse(localStorage.getItem('accounts')) || [];
+    var loggedInEmail = localStorage.getItem('loggedInEmail');
+    var account = existingData.find(account => account.email === loggedInEmail);
+    self.name= ko.observable('');
+    self.name(account.nome);
+    self.apelido= ko.observable('');
+    self.apelido(account.apelido);
+    self.telefone= ko.observable('');
+    self.telefone(account.telefone);
+    self.email= ko.observable('');
+    self.email(account.email);
+    self.nif= ko.observable('');
+    self.nif(account.nif);
+    self.rua= ko.observable('');
+    self.rua(account.rua);
+    self.cidade= ko.observable('');
+    self.cidade(account.cidade);
+    self.Distrito= ko.observable('');
+    self.Distrito(account.Distrito);
+    self.cp1= ko.observable('');
+    self.cp1(account.cp1);
+    self.cp2= ko.observable('');
+    self.cp2(account.cp2);
+    
+    self.account(account);
+    self.animals(account.animals);
+    console.log(self.animals(),self.account(), loggedInEmail)
 
     $('form').on('submit', function(event) {
-        console.log()
         event.preventDefault();
         var formData = {}; 
-        var url="";
         $(this).find('input, select').each(function() {
             formData[$(this).attr('id')] = $(this).val();
             console.log(formData)
