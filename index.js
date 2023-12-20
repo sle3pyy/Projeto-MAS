220a2274ed5c95a6b57dbbfbdb255c0ecd311a0c
sle3pyy <simaomarquesantos@gmail.com>
2023-12-20 20:01:05 +0000

    ye

--- index.js
+++ index.js
@@ -4,7 +4,31 @@ var vm= function(){
     self.petPhotos = ko.observableArray([]);
     self.loggedInEmail = localStorage.getItem('loggedInEmail');
     console.log(self.loggedInEmail);
+    if (localStorage.getItem('loggedInEmail')) {
+        $('a[href="log-in.html"]').html('<i class="fa fa-user"></i>').attr('href', 'petReg.html');
+        $('a[href="sign-in.html"]').text('Log out').attr('href', 'index.html').attr('id', 'logout');
+        
+        self.loggedInEmail = localStorage.getItem('loggedInEmail');
+        var existingData = JSON.parse(localStorage.getItem('accounts')) || [];
+        var account = existingData.find(account => account.email === self.loggedInEmail);
+        if (account && account.animals) {
+            self.animals(account.animals);
+    
+        }
+        console.log('Logged in!');
+        console.log(self.animals())
+    }
+    else {
+        console.log('Not logged in!');
+    }
+    
+    $('#logout').on('click', function(event) {
+        event.preventDefault();
+        localStorage.removeItem('loggedInEmail');
+        window.location.href = 'index.html'; 
+    });
 };
+
 $(document).ready(function() {
     var viewModel = new vm();
     ko.applyBindings(viewModel);
--- logged.js
+++ logged.js
@@ -1,16 +1,8 @@
 if (localStorage.getItem('loggedInEmail')) {
-    $('a[href="log-in.html"]').html('<i class="fas fa-user"></i>').attr('href', 'petReg.html');
+    $('a[href="log-in.html"]').html('<i class="fa fa-user"></i>').attr('href', 'petReg.html');
     $('a[href="sign-in.html"]').text('Log out').attr('href', 'index.html').attr('id', 'logout');
     
-    self.loggedInEmail = localStorage.getItem('loggedInEmail');
-    var existingData = JSON.parse(localStorage.getItem('accounts')) || [];
-    var account = existingData.find(account => account.email === self.loggedInEmail);
-    if (account && account.animals) {
-        self.animals(account.animals);
-
-    }
     console.log('Logged in!');
-    console.log(self.animals())
 }
 else {
     console.log('Not logged in!');
